3e80f60cf15f2a59887a14158425a56102370855
Navatar <umdomby@yandex.ru>
2025-06-07 21:16:24 +0300

    edit

--- docker-ardua-444/prisma/seed.ts
+++ docker-ardua-444/prisma/seed.ts
@@ -14,12 +14,6 @@ async function up() {
         role: 'ADMIN',
       },
       {
-        fullName: 'Yatsyk',
-        email: 'yatsyk@gmail.com',
-        password: hashSync('123123', 10),
-        role: 'USER',
-      },
-      {
         fullName: '123',
         email: '123@123.com',
         password: hashSync('123123', 10),
@@ -27,29 +21,12 @@ async function up() {
       },
     ],
   });
-  await prisma.category.createMany({
-    data: categories,
-  });
 
-  await prisma.product.createMany({
-    data: products,
-  });
-
-  await prisma.productItem.createMany({
-    data: productsItem,
-  });
-
-  await prisma.player.createMany({
-    data: players,
-  });
 }
 
 
 async function down() {
   await prisma.$executeRaw`TRUNCATE TABLE "User" RESTART IDENTITY CASCADE`;
-  await prisma.$executeRaw`TRUNCATE TABLE "Category" RESTART IDENTITY CASCADE`;
-  await prisma.$executeRaw`TRUNCATE TABLE "Product" RESTART IDENTITY CASCADE`;
-  await prisma.$executeRaw`TRUNCATE TABLE "ProductItem" RESTART IDENTITY CASCADE`;
 }
 
 
