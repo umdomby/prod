bb7c46770392861787c62fcea375a970f6f8994a
Navatar <umdomby@yandex.ru>
2025-06-18 11:59:25 +0300

    edit

--- docker-ardua-444/app/actions.ts
+++ docker-ardua-444/app/actions.ts
@@ -145,33 +145,35 @@ export async function addDevice(idDevice: string, autoConnect: boolean = false, 
 
 // Удаление устройства
 export async function deleteDevice(idDevice: string) {
-  try {
-    const session = await getUserSession();
-    if (!session) {
-      throw new Error('Пользователь не аутентифицирован');
-    }
-    const parsedIdDevice = deviceIdSchema.safeParse(idDevice);
-    if (!parsedIdDevice.success) {
-      throw new Error(parsedIdDevice.error.errors[0].message);
-    }
-    const userId = parseInt(session.id);
-    const device = await prisma.devices.findUnique({
-      where: { idDevice: parsedIdDevice.data },
-    });
-    if (!device || device.userId !== userId) {
-      throw new Error('Устройство не найдено или доступ запрещён');
-    }
-    await prisma.settings.deleteMany({
-      where: { devicesId: device.id },
-    });
-    await prisma.devices.delete({
-      where: { idDevice: parsedIdDevice.data },
-    });
-    revalidatePath('/');
-  } catch (error: unknown) {
-    console.error(`Ошибка в deleteDevice для idDevice=${idDevice}:`, error);
-    throw error; // Пробрасываем ошибку для обработки на клиенте
+  const session = await getUserSession();
+  if (!session) {
+    throw new Error('Пользователь не аутентифицирован');
   }
+
+  const parsedIdDevice = deviceIdSchema.safeParse(idDevice);
+  if (!parsedIdDevice.success) {
+    throw new Error(parsedIdDevice.error.errors[0].message);
+  }
+
+  const userId = parseInt(session.id);
+
+  const device = await prisma.devices.findUnique({
+    where: { idDevice: parsedIdDevice.data },
+  });
+
+  if (!device || device.userId !== userId) {
+    throw new Error('Устройство не найдено или доступ запрещен');
+  }
+
+  await prisma.settings.deleteMany({
+    where: { devicesId: device.id },
+  });
+
+  await prisma.devices.delete({
+    where: { idDevice: parsedIdDevice.data },
+  });
+
+  revalidatePath('/');
 }
 
 // Обновление настроек устройства
@@ -774,7 +776,12 @@ export async function getSavedRoomWithDevice(deviceId: string) {
   });
 
   if (!device) {
-    throw new Error('Устройство не найдено');
+    return {
+      id: null,
+      isDefault: false,
+      autoConnect: false,
+      deviceId: null,
+    };
   }
 
   const room = await prisma.savedRoom.findFirst({
