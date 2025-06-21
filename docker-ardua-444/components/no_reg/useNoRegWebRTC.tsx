1b8a59413f3514d7dd085fdda4b3da740091b383
Navatar <umdomby@yandex.ru>
2025-06-21 14:16:38 +0300

    edit

--- docker-ardua-444/components/no_reg/useNoRegWebRTC.tsx
+++ docker-ardua-444/components/no_reg/useNoRegWebRTC.tsx
@@ -668,9 +668,9 @@ export default function UseNoRegWebRTC({ roomId }: NoRegWebRTCProps) {
                 muted={false}
                 className="w-full h-full"
             />
-            <div className="absolute top-2 left-2 bg-black bg-opacity-50 text-white px-2 py-1 rounded">
-                Статус: {isConnected ? (isInRoom ? 'В комнате' : 'Подключено') : 'Отключено'} | ICE: {connectionState.ice || 'N/A'} | Signaling: {connectionState.signaling || 'N/A'}
-            </div>
+            {/*<div className="absolute top-2 left-2 bg-black bg-opacity-50 text-white px-2 py-1 rounded">*/}
+            {/*    Статус: {isConnected ? (isInRoom ? 'В комнате' : 'Подключено') : 'Отключено'} | ICE: {connectionState.ice || 'N/A'} | Signaling: {connectionState.signaling || 'N/A'}*/}
+            {/*</div>*/}
         </div>
     );
 }
